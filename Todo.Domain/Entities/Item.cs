2e93c5c669e2e005f77b740fa2312b9364bb37c2
wesleyalgorama <wesley@algorama.com.br>
2025-01-03 16:33:17 -0300

    feat: add entities

---
+++ Todo.Domain/Entities/Item.cs
@@ -0,0 +1,24 @@
+using Todo.Domain.Enums;
+
+namespace Todo.Domain.Entities;
+
+public class Item
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Status Status { get; set; }
+
+    public Item(string title, string description)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Description = description;
+        Status = Status.Todo;
+    }
+
+    public void ChangeStatus(Status status)
+    {
+        Status = status;
+    }
+}
---
+++ Todo.Domain/Enums/Status.cs
@@ -0,0 +1,8 @@
+namespace Todo.Domain.Enums;
+
+public enum Status
+{
+    Todo,
+    InProgress,
+    Done
+}
